e52ad227abcee084a205ae1d57222eeea6b72f77
Carlos Jiménez Delgado <mail@carlosjdelgado.com>
2016-07-01 14:34:02 +0200

    Add assembly information

--- GeoInfo/Properties/AssemblyInfo.cs
+++ GeoInfo/Properties/AssemblyInfo.cs
@@ -7,13 +7,10 @@ using Microsoft.Practices.Unity;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GeoInfo")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Powerful geo political data library")]
+[assembly: AssemblyCompany("Carlos Jiménez Delgado [www.carlosjdelgado.com]")]
 [assembly: AssemblyProduct("GeoInfo")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en-US")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -33,8 +30,14 @@ using Microsoft.Practices.Unity;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+#if DEBUG
+[assembly: AssemblyInformationalVersion("0.1.0-rc1")]
+#else
+[assembly: AssemblyInformationalVersion("0.1.0")]
+#endif
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+
 
 [assembly: PreApplicationStartMethod(typeof(GeoInfo.Properties.Startup), "Start")]
 namespace GeoInfo.Properties
